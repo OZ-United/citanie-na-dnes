4971a5af7c388446584951b4217f8ba42b4e907c
Jan Antala <jan@antala.sk>
2013-06-22 22:39:31 +0200

    fetch reflection

--- routes/reflections.js
+++ routes/reflections.js
@@ -1,8 +1,32 @@
 
 /*
- * GET home page.
+ * REFLECTIONS
  */
 
 exports.fetch = function(req, res, next){
-  console.log('fetch');
+  var jsdom = require("jsdom");
+
+  jsdom.env(
+    "http://baptist.sk/chlieb-nas-kazdodenny",
+    ["http://code.jquery.com/jquery.js"],
+    function (errors, window) {
+      var $ = window.$;
+
+      var articleDOM = window.$(".mainbody table div");
+      var html = $(articleDOM[0]).html();
+
+      var strdate = $($(articleDOM[0]).find('strong')[0]).text().replace(/(\s)/g, ' ').trim();
+      var title = $($(articleDOM[0]).find('strong')[1]).text().replace(/(\s)/g, ' ').trim();
+      var thought = $($(articleDOM[0]).find('strong')[2]).text().replace(/(\s)/g, ' ').trim();
+
+      console.log(html);
+      console.log(strdate);
+      console.log(title);
+      console.log(thought);
+
+      if (req && res) {
+        res.send(200);
+      }
+    }
+  );
 };
