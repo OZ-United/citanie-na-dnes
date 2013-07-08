5961ab186506bda31de6ff9a1e5545f2e5f2c9c5
Jan Antala <jan@antala.sk>
2013-07-08 09:49:06 +0200

    dashboard view - add user

--- dashboard/app/scripts/controllers/dashboard.js
+++ dashboard/app/scripts/controllers/dashboard.js
@@ -3,4 +3,11 @@
 angular.module('dashboardApp')
 .controller('DashboardCtrl', function ($scope, Users) {
   $scope.users = Users.query();
+
+  $scope.addUser = function(){
+    if ($scope.addUserForm.$valid) {
+      $scope.users.push($scope.user);
+      $scope.user = {};
+    }
+  };
 });
