240bd85c8cc45d392f90514272598126af9be9b0
Jan Antala <jan@antala.sk>
2014-02-11 12:48:24 +0100

    chore(angular): update to angular 1.2

--- dashboard/app/scripts/app.js
+++ dashboard/app/scripts/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
-angular.module('dashboardApp', ['ngResource'])
+angular.module('dashboardApp', ['ngResource', 'ngRoute'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/auth', {
-        templateUrl: '/views/main.html',
+        templateUrl: '/views/auth.html',
         controller: 'MainCtrl'
       })
       .when('/', {
