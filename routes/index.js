349b08a9339bf70864cd68b3eba4cd414ea86670
Jan Antala <jan@antala.sk>
2014-03-05 16:34:50 +0100

    feat(history): search in the history

--- routes/index.js
+++ routes/index.js
@@ -11,17 +11,37 @@ exports.index = function(req, res, next){
   ReflectionModel
   .find()
   .sort('-date')
+  .limit(1)
   .exec(function(err, reflections){
     if (err) { return next(error); }
-    var title = [];
-    if (req.query.all) {
-      title = reflections;
-    }
-    else if (reflections.length) {
-      title.push(reflections[0]);
-    }
+    res.render('index', { reflections: reflections, home: true});
+  });
+};
 
-    res.render('index', { reflections: title });
+exports.history = function(req, res, next){
+
+  var query = {};
+  if (req.query.date) {
+    var date = new Date(req.query.date);
+    query.date = {
+      '$gte': date,
+      '$lte': new Date(date.getTime() + (24 * 60 * 60 * 1000))
+    };
+  }
+  else if (req.query.start || req.query.end) {
+    query.date = {
+      '$gte': req.query.start ? new Date(req.query.start) : new Date(0),
+      '$lte': req.query.end ? new Date(req.query.end) : new Date()
+    };
+  }
+  console.log(query);
+
+  ReflectionModel
+  .find(query)
+  .sort('-date')
+  .exec(function(err, reflections){
+    if (err) { return next(error); }
+    res.render('index', { reflections: reflections, home: false });
   });
 };
 
